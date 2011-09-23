66c9c02fc0a1e462cd918185cc170d26ae11a380
andre.mussche@gmail.com <andre.mussche@gmail.com@de8d1014-7306-b6f0-bb50-032eed850de4>
2011-09-23 14:06:52 +0000

    branch version 2

---
+++ branches/version2/smmStatistics.pas
@@ -0,0 +1,13 @@
+unit smmStatistics;
+
+interface
+
+type
+  TMemoryStatistics = record
+    //number of allocs, reallocs, free's
+    //total memory allocated, header overhead, unused size overhead, etc
+  end;
+
+implementation
+
+end.
